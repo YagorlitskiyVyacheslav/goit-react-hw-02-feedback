708c063f8834d79a9641a15493505dadf0e57f4d
Vyacheslav <slavasan05@gmail.com>
2020-10-04 04:27:45 +0200

    Updates

---
+++ precache-manifest.c1a7cbf03a54cf6b31f6b72357fe0cec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53796aecb513ecbdd71fd77dded5ff9d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c632f6f654b12e124389",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a628aeb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a628aeb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fba1880b226780fe2c5",
+    "url": "/goit-react-hw-02-feedback/static/js/main.df3230c9.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
